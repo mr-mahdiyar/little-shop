c2f5b51d16315bd4913104ea244caea7e22bf766
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-06-16 13:41:15 +0330

    add: augment user, debug and implement credential login method

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,33 +1,60 @@
 import { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
+declare module "next-auth" {
+  interface User {
+    accessToken: string;
+    refreshToken: string;
+  }
+}
+
 export const authConfig: NextAuthConfig = {
   providers: [
     Credentials({
+      name: "credentials",
       credentials: {
         username: {},
         password: {},
       },
 
       authorize: async (credentials) => {
-
-        const url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000" + "/auth/login";
-        
+        const url = (process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000") + "/user/login";
+        console.log(url);
         const userInResponseFormat = await fetch(url, {
           method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
           body: JSON.stringify({
-            ...credentials,
-            expiresInMins: 3,
+            username: credentials.username as string,
+            password: credentials.password as string,
           }),
-          credentials: "include",
+          cache: "no-cache",
         });
 
-        const userInJSONFormat = await userInResponseFormat.json()
-
-        if(!!!userInJSONFormat) return null;
-        
-        return userInJSONFormat;
+        const userInJSONFormat = await userInResponseFormat.json();
+        console.log("user data: ", userInJSONFormat);
+        if (!!!userInJSONFormat) return null;
+        return { ...userInJSONFormat };
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user && "accessToken" in user && "refreshToken" in user) {
+        token.accessToken = user.accessToken;
+        token.refreshToken = user.refreshToken;
+      }
+      return token;
+    },
+    async session({ token, session }) {
+      if (!!!token) return session;
+
+      if ("accessToken" in token && "refreshToken" in token) {
+        session.user.accessToken = token.accessToken as string;
+        session.user.refreshToken = token.refreshToken as string;
+      }
+      return session;
+    },
+  },
 };
