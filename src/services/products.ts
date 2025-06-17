84cf13dba5b82a20e62d0b0015539c4643bc0b92
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-06-17 15:59:38 +0330

    add: output type for get products function

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,6 +1,7 @@
+import { Products } from "@/types/products";
 import apiCaller from "./api-caller";
 
 export async function getProducts() {
-  const response = await apiCaller.get("/products");
+  const response = await apiCaller.get<Products>("/products");
   return response;
 }
