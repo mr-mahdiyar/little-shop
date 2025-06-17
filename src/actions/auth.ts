27c733975752993c18198a516a93636460b127be
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-06-17 09:08:09 +0330

    add: sign out functionality

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,10 +1,11 @@
 "use server";
 
-import { auth, signIn } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 
 export async function login(credentials: { username: string | null; password: string | null }) {
   await signIn("credentials", { ...credentials, redirectTo: "/dashboard" });
 }
+
 export async function getSession() {
   return await auth();
 }
@@ -15,8 +16,13 @@ export async function getAccessToken() {
 
   return session.user.accessToken;
 }
+
 export async function authenticationChecking() {
   const session = await getSession();
   if (!!session?.user?.accessToken) return true;
   return false;
 }
+
+export async function logout() {
+  await signOut({redirectTo: "/auth/login"});
+} 
