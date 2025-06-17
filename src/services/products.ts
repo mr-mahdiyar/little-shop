c1f60587f9bf81c2636a9494756eca8ce1d4f2a0
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-06-17 09:08:51 +0330

    add: get products function

---
+++ src/services/products.ts
@@ -0,0 +1,7 @@
+import { Product } from "@/types/products";
+import apiCaller from "./api-caller";
+
+export async function getProducts() {
+  const response = await apiCaller.get("/products");
+  return response;
+}
