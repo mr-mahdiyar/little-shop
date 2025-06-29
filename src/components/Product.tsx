51076ca36edadcd24f82623e9aad546b3914c6e5
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-06-29 10:24:16 +0330

    add: product component

---
+++ src/components/Product.tsx
@@ -0,0 +1,17 @@
+import { Product } from "@/types/products";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Product({ category: { name: category }, images, price, slug, title }: Product) {
+  return (
+    <Link
+      href={`/products/${slug}`}
+      className="flex flex-col w-[350px] items-center p-4 rounded-xl bg-gray-300 gap-y-2"
+    >
+      <Image src={images[0]} alt={title} width={400} height={600} className="aspect-square w-full" />
+      <h1>{title}</h1>
+      <h2>{category}</h2>
+      <h3>{price}$</h3>
+    </Link>
+  );
+}
