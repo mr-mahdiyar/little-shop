7cc13b70d8f0829262afa69396b21ab94921ed89
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-06-29 10:02:09 +0330

    add: trusted hosts for images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placeimg.dev",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
