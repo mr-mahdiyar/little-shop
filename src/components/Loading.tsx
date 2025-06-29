125df2fa16f698834b58aae8a04c3ad31067a4e5
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-06-29 14:14:58 +0330

    feat: add class name to props

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,3 +1,7 @@
-export default function Loading() {
-  return <section className="loading" />;
+import { type ComponentPropsWithRef } from "react";
+
+type LoadingProps = Pick<ComponentPropsWithRef<"section">, "className">;
+
+export default function Loading({ ...props }: LoadingProps) {
+  return <section className={`loading ${props.className || null}`} />;
 }
