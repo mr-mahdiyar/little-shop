afadfc8d81a953568b26a9babf9438d641531eb9
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-06-30 11:21:35 +0330

    feat: filtering products by title

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -6,10 +6,24 @@ import useUser from "@/hooks/useUser";
 import Loading from "@/components/Loading";
 import Product from "@/components/Product";
 import Container from "@/components/Container";
+import Image from "next/image";
+import logoutIcon from "@/../public/icons/logout.png";
+import SearchBox from "@/components/SearchBox";
+import { useSearchTermsAndValues } from "@/hooks/useSearchTermsAndValues";
+import useSearchEndPintCreator from "@/hooks/useSearchEndPointCreator";
+import useFilteredProducts from "@/hooks/useFilteredProducts";
 
 export default function DashboardPage() {
-  const { products, error, isError: isProductsError, isLoading: isProductsLoading } = useProducts();
+  
+  const searchTermsAndValues = useSearchTermsAndValues("title");
+  const searchEndPoint = useSearchEndPintCreator(searchTermsAndValues);
+
+  const { filteredProducts } = useFilteredProducts(searchEndPoint);
+
+  const { products: allProducts, error, isError: isProductsError, isLoading: isProductsLoading } = useProducts();
   const { isLoading: isUserDataLoading, userData } = useUser();
+
+  const products = searchEndPoint ? filteredProducts : allProducts;
 
   if (isProductsLoading) {
     return (
@@ -29,12 +43,15 @@ export default function DashboardPage() {
     <Container className="flex flex-col gap-y-4">
       <div className="flex mx-auto justify-between mt-10 items-center w-full">
         {isUserDataLoading ? <Loading /> : <h1>Welcome {userData?.name}</h1>}
+        <SearchBox searchTerm="title" placeholder="search products..." className="w-72" />
         <form
           action={async () => {
             await logout();
           }}
         >
-          <button className="bg-red-500 text-white p-0.5 rounded-md hover:bg-red-400 cursor-pointer">logout</button>
+          <button className="cursor-pointer">
+            <Image src={logoutIcon} alt="logout" className="w-8" />
+          </button>
         </form>
       </div>
       <section className="flex flex-wrap justify-between gap-y-8">
