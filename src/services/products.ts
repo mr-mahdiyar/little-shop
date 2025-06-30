91101a429f27f8b1c52f2f970df62d7bf92b98d4
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-06-30 11:20:52 +0330

    feat: add filterProducts function

--- src/services/products.ts
+++ src/services/products.ts
@@ -5,3 +5,10 @@ export async function getProducts() {
   const response = await apiCaller.get<Array<Product>>("/products");
   return response;
 }
+
+export async function filterProducts(term: string) {
+  const endPoint = "/products" + "?" + term;
+  console.log("end point: ", endPoint)
+  const response = await apiCaller.get<Array<Product>>(endPoint);
+  return response;
+}
